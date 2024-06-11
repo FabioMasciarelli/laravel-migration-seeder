39f10ca51d752a9106a5f20b83702201c124af3b
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-11 16:11:54 +0200

    add: HomeController, HomeModel and print in Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
     <h1>Home page</h1>
+
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Numero</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">Numero di Carrozze</th>
+            </tr>
+        </thead>
+        @foreach ($trains as $train)
+            <tbody>
+                <tr>
+                    <th scope="row">{{ $train->id }}</th>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_di_partenza }}</td>
+                    <td>{{ $train->stazione_di_arrivo }}</td>
+                    <td>{{ $train->orario_di_partenza }}</td>
+                    <td>{{ $train->orario_di_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                </tr>
+            </tbody>
+        @endforeach
+    </table>
 @endsection
